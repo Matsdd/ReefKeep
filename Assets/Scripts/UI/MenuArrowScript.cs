c292d9ff67127506c2c9ae211578a495eb36fa9f
RikWijtman <1045768@hr.nl>
2024-04-10 11:18:54 +0200

    wat  buttons enzo

--- Assets/Scripts/UI/MenuArrowScript.cs
+++ Assets/Scripts/UI/MenuArrowScript.cs
@@ -6,20 +6,34 @@ public class MenuArrowScript : MonoBehaviour
 {
     public GameObject UI;
     public bool folded = false;
+    public float foldTimer = 0;
 
     public void onClick()
     {
-        if (folded)
+        if (folded && foldTimer == 0)
         {
-            UI.transform.position -= new Vector3(100, 0, 0);
+            foldTimer = 250;
             folded = false;
-            Debug.Log("Kaas");
         }
-        else
+        
+        if (!folded && foldTimer == 0)
         {
-            UI.transform.position += new Vector3(100, 0, 0);
+            foldTimer = -250;
             folded = true;
-            Debug.Log("Kaas");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (foldTimer > 0)
+        {
+            UI.transform.position += new Vector3(12.5f, 0, 0);
+            foldTimer -= 12.5f;
+        }
+        else if (foldTimer < 0)
+        {
+            UI.transform.position += new Vector3(-12.5f, 0, 0);
+            foldTimer += 12.5f;
         }
     }
 }
