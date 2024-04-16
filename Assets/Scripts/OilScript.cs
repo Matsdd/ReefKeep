43cae094b704d0085274a2d37114dc4020962f64
RikWijtman <1045768@hr.nl>
2024-04-16 15:23:06 +0200

    trash bah bleh

--- Assets/Scripts/OilScript.cs
+++ Assets/Scripts/OilScript.cs
@@ -6,10 +6,21 @@ public class OilScript : MonoBehaviour
 {
     public GameObject oil;
     public GameObject oilBit;
+
     private void Start()
     {
-        oilBit = Instantiate(oil, new Vector3(0,0,0), new Quaternion(0,0,0,0));
-        oilBit.transform.parent = this.transform;
-        Debug.Log("nee");
+        float size = Mathf.Round(Random.Range(2, 8));
+        for (int i = 0; i  < size; i++)
+        {
+
+            float oilx = Random.Range(size * -0.3f, size * 0.3f);
+            float oily = Random.Range(size * -0.2f, size * 0.2f);
+
+            oilBit = Instantiate(oil, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0));
+            oilBit.transform.parent = this.transform;
+
+            oilBit.transform.position = new Vector3(oilx, oily, 0);
+        }
+        
     }
 }
--- Assets/Scripts/TrashScript.cs
+++ Assets/Scripts/TrashScript.cs
@@ -5,27 +5,32 @@ using UnityEngine;
 public class TrashScript : MonoBehaviour
 {
     public GameObject trash;
+    GameObject obj;
 
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-
-            if (hit.collider != null && hit.collider.gameObject.tag == "Trash")
+            if (hit)
             {
-                Destroy(trash);
+                obj = hit.transform.gameObject;
+            }
+
+            if (hit.collider != null && hit.collider.gameObject.tag == "Trash" && obj != null)
+            {
+                Destroy(obj);
             }
         }
     }
 
     float speed = 2f;
-    float height = 0.2f;
+    float height = 0.01f;
 
     private void FixedUpdate()
     {
         Vector3 pos = transform.position;
-        float newY = Mathf.Sin(Time.time * speed) * height;
+        float newY = Mathf.Sin(Time.time * speed) * height + pos.y;
         transform.position = new Vector3(pos.x, newY, pos.z);
     }
 }
