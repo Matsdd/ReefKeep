616c07dac11091c934a1011928469d7565463a66
RikWijtman <1045768@hr.nl>
2024-04-17 15:49:09 +0200

    bug fixes

--- Assets/Scripts/OilScript.cs
+++ Assets/Scripts/OilScript.cs
@@ -5,18 +5,14 @@ public class OilScript : MonoBehaviour
     public GameObject oil;
     public GameObject oilParent;
 
+    float size;
+
     private void Start()
     {
-        float size = Mathf.Round(Random.Range(2, 4));
+        size = Mathf.Round(Random.Range(2, 4));
         for (int i = 0; i  < size; i++)
         {
-            float oilx = Random.Range(size * -0.3f, size * 0.3f);
-            float oily = Random.Range(size * -0.2f, size * 0.2f);
-
-            GameObject oilBit = Instantiate(oil, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0));
-            oilBit.transform.parent = this.transform;
-
-            oilBit.transform.position = new Vector3(oilx + this.transform.position.x, oily + this.transform.position.y, 0);
+            addChild();
         }
         
     }
@@ -27,21 +23,26 @@ public class OilScript : MonoBehaviour
         {
             float duplicate = Mathf.Round(Random.Range(0, 250));
             Debug.Log(duplicate);
-            float size = 1;
+            size = 1;
             if (duplicate == 1)
             {
-                float oilx = Random.Range(size * -0.3f, size * 0.3f);
-                float oily = Random.Range(size * -0.2f, size * 0.2f);
-
-                GameObject oilBit = Instantiate(oil, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0));
-                oilBit.transform.parent = this.transform;
-
-                oilBit.transform.position = new Vector3(oilx + this.transform.position.x, oily + this.transform.position.y, 0);
+                addChild();
             }
         }
         else
         {
             Destroy(oilParent);
         }
+    }
+
+    public void addChild()
+    {
+        float oilx = Random.Range(size * -0.3f, size * 0.3f);
+        float oily = Random.Range(size * -0.2f, size * 0.2f);
+
+        GameObject oilBit = Instantiate(oil, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0));
+        oilBit.transform.parent = this.transform;
+
+        oilBit.transform.position = new Vector3(oilx + this.transform.position.x, oily + this.transform.position.y, 0);
     }
 }
