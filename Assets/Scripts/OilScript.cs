81d1476ec800c4dfc34cb09c6ec6ac21ab60007f
RikWijtman <1045768@hr.nl>
2024-04-17 12:58:34 +0200

    trash fix

--- Assets/Scripts/OilScript.cs
+++ Assets/Scripts/OilScript.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class OilScript : MonoBehaviour
 {
     public GameObject oil;
+    public GameObject oilParent;
 
     private void Start()
     {
-        float size = Mathf.Round(Random.Range(2, 8));
+        float size = Mathf.Round(Random.Range(2, 4));
         for (int i = 0; i  < size; i++)
         {
 
@@ -16,8 +17,32 @@ public class OilScript : MonoBehaviour
             GameObject oilBit = Instantiate(oil, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0));
             oilBit.transform.parent = this.transform;
 
-            oilBit.transform.position = new Vector3(oilx, oily, 0);
+            oilBit.transform.position = new Vector3(oilx + this.transform.position.x, oily + this.transform.position.y, 0);
         }
         
+    }
+
+    private void FixedUpdate()
+    {
+        if (this.transform.childCount > 0)
+        {
+            float duplicate = Mathf.Round(Random.Range(0, 250));
+            Debug.Log(duplicate);
+            float size = 1;
+            if (duplicate == 1)
+            {
+                float oilx = Random.Range(size * -0.3f, size * 0.3f);
+                float oily = Random.Range(size * -0.2f, size * 0.2f);
+
+                GameObject oilBit = Instantiate(oil, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0));
+                oilBit.transform.parent = this.transform;
+
+                oilBit.transform.position = new Vector3(oilx + this.transform.position.x, oily + this.transform.position.y, 0);
+            }
+        }
+        else
+        {
+            Destroy(oilParent);
+        }
     }
 }
--- Assets/Scripts/TrashScript.cs
+++ Assets/Scripts/TrashScript.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class TrashScript : MonoBehaviour
 {
+    public GameObject oil;
     public GameObject trash;
     GameObject obj;
 
     float speed = 2f;
     float height = 0.01f;
+    public int sort = 0;
+    float side = 0;
 
     void Update()
     {
@@ -27,8 +30,22 @@ public class TrashScript : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //Set movement oil
         Vector3 pos = transform.position;
+        float newX = pos.x;
+        if (sort == 1)
+        {
+            if (side == 0)
+            {
+                side = Mathf.Round(Random.Range(-1.4f, 1.4f));
+            }
+
+            newX = pos.x + (side * 0.001f);
+        }
+
+
         float newY = Mathf.Sin(Time.time * speed) * height + pos.y;
-        transform.position = new Vector3(pos.x, newY, pos.z);
+        transform.position = new Vector3(newX, newY, pos.z);
+
     }
 }
