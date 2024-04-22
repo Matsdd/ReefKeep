36c294bce495284a0ebc62fc3e1cf8235f1356df
RikWijtman <1045768@hr.nl>
2024-04-22 14:55:50 +0200

    building changes

--- Assets/Scripts/buidligns/BuildingScript.cs
+++ Assets/Scripts/buidligns/BuildingScript.cs
@@ -5,19 +5,14 @@ using TMPro;
 
 public class BuildingScript : MonoBehaviour
 {
-    public string assignedBuilding;
-    public int buildingCost;
-
     public GameObject self;
     public Canvas canvas;
-    public TMP_Text CanvasTxt;
 
     private void OnMouseDown()
     {
         if (!canvas.enabled)
         {
             canvas.enabled = true;
-            CanvasTxt.text = "Would you like to build a "+assignedBuilding+" for "+buildingCost+" fishbucks?";
         }
     }
 }
