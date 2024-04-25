6d0e3fdf8b97365471be4e03c482aa98728d898a
Roel <roelhoogendoorn01369@gmail.com>
2024-04-25 20:16:22 +0200

    Fixed click through UI

--- Assets/Scripts/UI/buttonScript.cs
+++ Assets/Scripts/UI/buttonScript.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class ButtonScript : MonoBehaviour
 {
     public int scene;
 
-
     void OnMouseDown()
     {
         // Load scene
+        if (EventSystem.current.IsPointerOverGameObject()) return;
         SceneManager.LoadScene(scene, LoadSceneMode.Single);
     }
 }
