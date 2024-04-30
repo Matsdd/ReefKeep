8a4e570307290f3c6a6899d887d5a3eb467e030f
Roel <roelhoogendoorn01369@gmail.com>
2024-04-30 13:39:24 +0200

    Added OfflineTrash function

--- Assets/Scripts/Trash/TrashManagerScript.cs
+++ Assets/Scripts/Trash/TrashManagerScript.cs
@@ -8,6 +8,11 @@ public class TrashManagerScript : MonoBehaviour
 
     public static int trashCashMultiplier = 1;
 
+    private void Start()
+    {
+        AddOfflineTrash();
+    }
+
     void Update()
     {
         if (Input.GetMouseButton(0))
@@ -36,27 +41,66 @@ public class TrashManagerScript : MonoBehaviour
         float randomNum = Mathf.Round(Random.Range(0, 1500));
         if (randomNum == 1)
         {
-            float randomX = Random.Range(-10, 10);
-            float randomY = Random.Range(-12, 12);
 
-            // Generate a random number to choose trash type
-            randomNum = Mathf.Round(Random.Range(0, 5));
-            if (randomNum >= 4)
-            {
-                // Big trash
-                Instantiate(trashB, new Vector3(randomX, randomY, 0), Quaternion.identity);
-            }
-            else if (randomNum == 3)
-            {
-                // Oil
-                Instantiate(oil, new Vector3(randomX, randomY, 0), Quaternion.identity);
-            }
-            else
-            {
-                // Small Trash
-                Instantiate(trashS, new Vector3(randomX, randomY, 0), Quaternion.identity);
-            }
         }
     }
 
+    // Function to spawn trash
+    private void SpawnTrash()
+    {
+        float randomX = Random.Range(-10, 10);
+        float randomY = Random.Range(-12, 12);
+
+        // Generate a random number to choose trash type
+        float randomNum = Mathf.Round(Random.Range(0, 5));
+        if (randomNum >= 4)
+        {
+            // Big trash
+            Instantiate(trashB, new Vector3(randomX, randomY, 0), Quaternion.identity);
+        }
+        else if (randomNum == 3)
+        {
+            // Oil
+            Instantiate(oil, new Vector3(randomX, randomY, 0), Quaternion.identity);
+        }
+        else
+        {
+            // Small Trash
+            Instantiate(trashS, new Vector3(randomX, randomY, 0), Quaternion.identity);
+        }
+    }
+
+    // Add trash after the player has been offline for a while
+    private void AddOfflineTrash()
+    {
+        // Get the timestamp when the player last played
+        if (long.TryParse(PlayerPrefs.GetString("LastPlayTimeTicks"), out long lastPlayTicks))
+        {
+            // Get the current time
+            long currentTicks = System.DateTime.UtcNow.Ticks;
+
+            // Calculate the time difference in ticks
+            long timeDifferenceTicks = currentTicks - lastPlayTicks;
+
+            // Convert ticks to seconds
+            float timeDifferenceHours = (timeDifferenceTicks / (float)System.TimeSpan.TicksPerSecond) / 3600;
+
+            // Calculate the offline trash --------- MAYBE MAKE A BETTER FORMULA, BUT IM BAD AT MATH :(
+            int offlineTrash = Mathf.FloorToInt(timeDifferenceHours);
+
+            // Cap the max to 10
+            offlineTrash = Mathf.Min(offlineTrash, 10);
+            Debug.Log(offlineTrash);
+
+            // Add the offline income to the player's money
+            for (int i = 0; i < offlineTrash; i++)
+            {
+                SpawnTrash();
+            }
+        }
+        else
+        {
+            Debug.LogError("AddOfflineTrash FAIL!");
+        }
+    }
 }
