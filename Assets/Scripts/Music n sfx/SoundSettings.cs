4c6f6233515b9abd36dd840bde3b3316f2bf20d4
RikWijtman <1045768@hr.nl>
2024-05-14 10:11:27 +0200

    final commit

--- Assets/Scripts/Music n sfx/SoundSettings.cs
+++ Assets/Scripts/Music n sfx/SoundSettings.cs
@@ -6,32 +6,59 @@ public class SoundSettings : MonoBehaviour
 {
     //geen flauw idee wtf hier allemaal gebeurt
     [SerializeField] Slider soundSlider;
+    [SerializeField] Slider soundSliderSfx;
     [SerializeField] AudioMixer masterMixer;
+    [SerializeField] AudioMixer sfxMixer;
 
     private void Start()
     {
-        setVolume(PlayerPrefs.GetFloat("SavedMasterVolume", 100));
+        setVolume(PlayerPrefs.GetFloat("SavedMasterVolume", 100),0);
+        setVolume(PlayerPrefs.GetFloat("SavedSfxVolume", 100),1);
     }
 
-    public void setVolume(float _value)
+    public void setVolume(float _value, int sort)
     {
         if (_value < 1)
         {
             _value = .001f;
         }
 
-        refreshSlider(_value);
-        PlayerPrefs.SetFloat("SavedMasterVolume", _value);
-        masterMixer.SetFloat("MasterVolume", Mathf.Log10(_value / 100) * 20f);
+        refreshSlider(_value,sort);
+        if (sort == 0)
+        {
+
+            PlayerPrefs.SetFloat("SavedMasterVolume", _value);
+            masterMixer.SetFloat("MasterVolume", Mathf.Log10(_value / 100) * 20f);
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("SavedSfxVolume", _value);
+            sfxMixer.SetFloat("MasterVolume", Mathf.Log10(_value / 100) * 20f);
+        }
     }
 
-    public void setVolumeFromSlider()
+
+    public void setVolumeFromSlider(int sort)
     {
-        setVolume(soundSlider.value);
+        if (sort == 0)
+        {
+            setVolume(soundSlider.value,sort);
+        }
+        else
+        {
+            setVolume(soundSliderSfx.value,sort);
+        }
     }
 
-    public void refreshSlider(float _value)
+    public void refreshSlider(float _value, int sort)
     {
-        soundSlider.value = _value;
+        if (sort == 0)
+        {
+            soundSlider.value = _value;
+        }
+        else
+        {
+            soundSliderSfx.value = _value;
+        }
     }
 }
