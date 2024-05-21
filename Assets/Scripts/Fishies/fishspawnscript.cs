db9f71acb1cdc117c1170dd329d3b3f592fbbbab
S-witsel <stijnwitsel@gmail.com>
2024-05-21 09:41:08 +0200

    json stuff fixed
    AAAAAAAAAAAAAAAAAAAAA

--- Assets/Scripts/Fishies/fishspawnscript.cs
+++ Assets/Scripts/Fishies/fishspawnscript.cs
@@ -1,117 +1,194 @@
-using JetBrains.Annotations;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+using System.Collections.Generic;
+using System;
 
-[Serializable]
-public class FishObject
+public class FishSpawnScript : MonoBehaviour
 {
-    public string name;
-    public string xcordinate;
-    public string ycordinate;
-}
-
-[Serializable]
-public class FishInEcosystemData
-{
-    public List<FishObject> fishObjects;
-}
-
-public class fishspawnscript : MonoBehaviour
-{
-
-    public float spawnrate = 1;
+    public float spawnRate = 1;
     private float timer = 0;
-    private float randomstartpos = 0;
-    private float randomheightpos = 0;
 
-    public float leftborder = -30;
-    public float rightborder = 30;
-
+    public float leftBorder = -30;
+    public float rightBorder = 30;
     public float maxSpawnHeight = 13;
     public float minSpawnHeight = -13;
 
-    private GameObject chosenFish;
-    private int totalSpawnrange;
-
     public GameObject fish1;
     public int fish1SpawnWeight = 1;
-    private int spawn1Range;
 
     public GameObject fish2;
     public int fish2SpawnWeight = 1;
-    private int spawn2Range;
 
+    private List<FishObject> fishList = new();
+    private string filePath;
 
-    // Start is called before the first frame update
-    void Start()
+    private const int populationLimit = 20;
+
+    private void Start()
     {
-        spawn1Range = 0 + fish1SpawnWeight;
-        spawn2Range = spawn1Range + fish2SpawnWeight;
-
-        totalSpawnrange = spawn2Range;
+        filePath = Application.persistentDataPath + "/fishInEcosystem.json";
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        SceneManager.sceneUnloaded += OnSceneUnloaded;
+        LoadFishData();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (timer < spawnrate)
+        if (fishList.Count >= populationLimit)
         {
-            timer = timer + Time.deltaTime;
+            return; // Stop spawning if the population limit is reached
+        }
+
+        if (timer < spawnRate)
+        {
+            timer += Time.deltaTime;
         }
         else
         {
-            FishChooser();
-            randomstartpos = UnityEngine.Random.value;
-            randomheightpos = (UnityEngine.Random.value * 26) - 13;
-
-            if (randomstartpos < 0.5)
-            {
-                Instantiate(chosenFish, new Vector3(leftborder, randomheightpos, 0), transform.rotation);
-            }
-            else
-            {
-                Instantiate(chosenFish, new Vector3(rightborder, randomheightpos, 0), transform.rotation);
-            }
-
+            SpawnFish();
             timer = 0;
         }
     }
 
-    void FishChooser()
+    private void SpawnFish()
     {
-        float randomValue = UnityEngine.Random.Range(0, totalSpawnrange);
+        if (fishList.Count >= populationLimit)
+        {
+            Debug.Log("Population limit reached. No more fish will be spawned.");
+            return;
+        }
 
-        if (randomValue < spawn1Range)
+        GameObject chosenFish = UnityEngine.Random.Range(0, fish1SpawnWeight + fish2SpawnWeight) < fish1SpawnWeight ? fish1 : fish2;
+        float randomStartPos = UnityEngine.Random.Range(0f, 1f);
+        float randomHeightPos = UnityEngine.Random.Range(minSpawnHeight, maxSpawnHeight);
+        Vector3 spawnPosition = randomStartPos < 0.5f ? new Vector3(leftBorder, randomHeightPos, 0) : new Vector3(rightBorder, randomHeightPos, 0);
+        GameObject newFish = Instantiate(chosenFish, spawnPosition, Quaternion.identity);
+
+        // Add spawned fish to the list with instance ID
+        fishList.Add(new FishObject
         {
-            chosenFish = fish1;
+            name = chosenFish.name,
+            xposition = spawnPosition.x,
+            yposition = spawnPosition.y,
+            instanceID = newFish.GetInstanceID()
+        });
+        SaveFishData();
+    }
+
+    private void SaveFishData()
+    {
+        string jsonData = JsonUtility.ToJson(new FishInEcosystemData { fishObjects = fishList });
+        File.WriteAllText(filePath, jsonData);
+    }
+
+    private void LoadFishData()
+    {
+        if (File.Exists(filePath))
+        {
+            string jsonData = File.ReadAllText(filePath);
+            Debug.Log("Loaded JSON Data: " + jsonData);
+
+            try
+            {
+                FishInEcosystemData data = JsonUtility.FromJson<FishInEcosystemData>(jsonData);
+                if (data != null && data.fishObjects != null)
+                {
+                    fishList.Clear(); // Clear the current list
+
+                    foreach (FishObject fish in data.fishObjects)
+                    {
+                        Debug.Log("Loading fish: " + fish.name);
+                        GameObject fishPrefab = Resources.Load<GameObject>("Prefabs/Fishies/" + fish.name);
+
+                        if (fishPrefab != null)
+                        {
+                            Vector3 spawnPosition = new Vector3(fish.xposition, fish.yposition, 0);
+                            GameObject newFish = Instantiate(fishPrefab, spawnPosition, Quaternion.identity);
+
+                            // Add the newly spawned fish to the list with a new instance ID
+                            fishList.Add(new FishObject
+                            {
+                                name = fish.name,
+                                xposition = spawnPosition.x,
+                                yposition = spawnPosition.y,
+                                instanceID = newFish.GetInstanceID()
+                            });
+
+                            Debug.Log("Spawned fish: " + fish.name + " at position: " + spawnPosition);
+                        }
+                        else
+                        {
+                            Debug.LogError("Could not load prefab for fish: " + fish.name);
+                        }
+                    }
+
+                    SaveFishData(); // Save the updated list with new instance IDs
+                }
+                else
+                {
+                    Debug.LogError("Deserialized data is null or fishObjects list is null.");
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Error deserializing JSON data: " + e.Message);
+            }
         }
-        else if (randomValue < spawn2Range)
+        else
         {
-            chosenFish = fish2;
+            Debug.LogError("File not found: " + filePath);
         }
-        else if (randomValue < totalSpawnrange)
+    }
+
+    private void RemoveFishFromList(GameObject fishToRemove)
+    {
+        int fishInstanceID = fishToRemove.GetInstanceID();
+        FishObject fishObjectToRemove = fishList.Find(fish => fish.instanceID == fishInstanceID);
+        if (fishObjectToRemove != null)
         {
-            chosenFish = fish2;
+            fishList.Remove(fishObjectToRemove);
+            SaveFishData();
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        // Check if the exiting object has a specific tag (optional)
         if (other.CompareTag("Fish"))
         {
-            Debug.Log("hooray!!: " + other.gameObject.GetInstanceID());
-            // Destroy the exiting object
+            RemoveFishFromList(other.gameObject);
             Destroy(other.gameObject);
-
-        }
-        else
-        {
-            Debug.Log("collider has wrong tag");
         }
     }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        LoadFishData();
+    }
+
+    private void OnSceneUnloaded(Scene scene)
+    {
+        SaveFishData();
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        SceneManager.sceneUnloaded -= OnSceneUnloaded;
+    }
+}
+
+[Serializable]
+public class FishInEcosystemData
+{
+    public List<FishObject> fishObjects = new List<FishObject>();
+}
+
+[Serializable]
+public class FishObject
+{
+    public string name;
+    public float xposition;
+    public float yposition;
+    public int instanceID;
 }
