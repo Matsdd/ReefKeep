70d5f8094b9b6707a96ce83eb1521a0c4a93cf89
RikWijtman <1045768@hr.nl>
2024-05-28 16:42:13 +0200

    fffffffffffffffffffffffff

--- Assets/Scripts/Fishies/JellySpawnerScript.cs
+++ Assets/Scripts/Fishies/JellySpawnerScript.cs
@@ -17,13 +17,15 @@ public class JellySpawnerScript : MonoBehaviour
         {
             //spawn jellyfish 1 or 2
             rand = Random.Range(0, 2);
+            //determine location it spawns
+            Vector3 newPos = new Vector3(Random.Range(-14,14),Random.Range(-12,12), 1);
             if (rand == 1)
             {
-                Instantiate(jelly2);
+                Instantiate(jelly2,newPos,new Quaternion(0,0,0,0));
             }
             else
             {
-                Instantiate(jelly1);
+                Instantiate(jelly1, newPos, new Quaternion(0, 0, 0, 0));
             }
             //set total of jellyfih
             totalJellies++;
