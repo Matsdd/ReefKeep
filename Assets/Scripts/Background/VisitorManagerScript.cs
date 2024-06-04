707df26bcdb34b86acbb2d7b702aced3bb0eab97
RikWijtman <1045768@hr.nl>
2024-06-04 15:54:25 +0200

    oelala

---
+++ Assets/Scripts/Background/VisitorManagerScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisitorManagerScript : MonoBehaviour
+{
+    public GameObject pers1;
+    public GameObject pers2;
+    private void FixedUpdate()
+    {
+        //spawn visitors depending on level
+        if (PlayerPrefs.GetInt("VisitorCenterLevel") == 1)
+        {
+            int rand = Random.Range(0, 500);
+            if (rand == 1)
+            {
+                Instantiate(pers1);
+            }else if (rand == 2)
+            {
+                Instantiate(pers2);
+            }
+        }else if (PlayerPrefs.GetInt("VisitorCenterLevel") == 2)
+        {
+            int rand = Random.Range(0, 350);
+            if (rand == 1)
+            {
+                Instantiate(pers1);
+            }
+            else if (rand == 2)
+            {
+                Instantiate(pers2);
+            }
+        }
+        else
+        {
+            int rand = Random.Range(0, 200);
+            if (rand == 1)
+            {
+                Instantiate(pers1);
+            }
+            else if (rand == 2)
+            {
+                Instantiate(pers2);
+            }
+        }
+    }
+}
