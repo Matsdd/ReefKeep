1469ec5cd95dbfbb5480b5c70850c96d62143cea
RikWijtman <1045768@hr.nl>
2024-06-19 19:37:15 +0200

    Jellyfish spawn fix

--- Assets/Scripts/Fishies/JellySpawnerScript.cs
+++ Assets/Scripts/Fishies/JellySpawnerScript.cs
@@ -9,26 +9,39 @@ public class JellySpawnerScript : MonoBehaviour
     public GameObject jelly1;
     public GameObject jelly2;
 
+    private void Start()
+    {
+        for (int i = 0; i < totalJellies; i++)
+        {
+            spawnJelly();
+        }
+    }
+
     private void FixedUpdate()
     {
         //spawnt jellyfish?
         int rand = Random.Range(0, 5000);
         if (rand == 1 && totalJellies <= 5)
         {
-            //spawn jellyfish 1 or 2
-            rand = Random.Range(0, 2);
-            //determine location it spawns
-            Vector3 newPos = new Vector3(Random.Range(PlayerPrefs.GetInt("BigTrash1") == 1 ? -14 : -24, PlayerPrefs.GetInt("BigTrash2") == 1 ? 14 : 24),Random.Range(-12,12), 1);
-            if (rand == 1)
-            {
-                Instantiate(jelly2,newPos,new Quaternion(0,0,0,0));
-            }
-            else
-            {
-                Instantiate(jelly1, newPos, new Quaternion(0, 0, 0, 0));
-            }
+            spawnJelly();
             //set total of jellyfih
             totalJellies++;
+        }
+    }
+
+    private void spawnJelly()
+    {
+        //spawn jellyfish 1 or 2
+        int rand = Random.Range(0, 2);
+        //determine location it spawns
+        Vector3 newPos = new Vector3(Random.Range(PlayerPrefs.GetInt("BigTrash1") == 1 ? -14 : -24, PlayerPrefs.GetInt("BigTrash2") == 1 ? 14 : 24), Random.Range(-12, 12), 1);
+        if (rand == 1)
+        {
+            Instantiate(jelly2, newPos, new Quaternion(0, 0, 0, 0));
+        }
+        else
+        {
+            Instantiate(jelly1, newPos, new Quaternion(0, 0, 0, 0));
         }
     }
 }
